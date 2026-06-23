695d495bdabc87cd839a4e219ccbd95ec3136083
Anjum Misbah Z <mizzumisbah3@gmail.com>
2026-06-23 13:33:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+window.addEventListener("scroll", () => {
+
+    const navbar =
+        document.getElementById("navbar");
+
+    if (window.scrollY > 50) {
+
+        navbar.classList.add(
+            "nav-scrolled");
+
+    }
+    else {
+
+        navbar.classList.remove(
+            "nav-scrolled");
+
+    }
+
+});
